a4b98f463719f104833c85891326598c23decf3a
TK <TK@gmail.com>
2023-06-01 16:40:05 +0800

    Commit code. Update time: 2023-06-01

--- JavaProjectDemo1/platforms/android/mylibrary/src/main/java/com/qiniu/android/http/request/handler/RequestShouldRetryHandler.java
+++
@@ -1,9 +0,0 @@
-package com.qiniu.android.http.request.handler;
-
-import com.qiniu.android.http.ResponseInfo;
-
-import org.json.JSONObject;
-
-public interface RequestShouldRetryHandler {
-    boolean shouldRetry(ResponseInfo responseInfo, JSONObject response);
-}
